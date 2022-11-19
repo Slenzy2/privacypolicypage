39234fa6942191f0749f514487415ac3fd507386
Ikupolati Tobi <94957025+Slenzy2@users.noreply.github.com>
2022-11-19 23:43:56 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,25 +29,11 @@ module.exports = {
 
         xxsx: { max: "445px" },
       },
-      fontSize: {
-        "6xl": "3.5rem",
-      },
-      zIndex: {
-        1000: 1000,
-      },
-      colors: {
-        primary: "#a02279",
-        bordercolor: "#a3a3a3",
-        input: "rgba(247, 247, 247, 0.1)",
-        placeholdertext: "#b8b8b8",
-        token: "#f7f7f7",
-        collection: "#d7d7d7",
-        collection_bg:
-          "linear-gradient(124.4deg, #ffffff 10.8%, #ffffff 87.34%)",
-        tinytext: "#434343",
-        footer: "#1d1d1e",
-        secondary: "linear-gradient(90deg, #a02279 11.45%, #a02279 11.45%)",
-      },
+      fontFamily: {
+        avenir: ['Avenir'],
+        avenir_light: ['Avenir-light'],
+        avenir_bold: ['Avenir-bold']
+      }
     },
   },
   plugins: [],
